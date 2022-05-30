439ac7c29f628a44beda14a74ea386c3cf98e0f9
Alexis Navarro <alexisnavarro1994@gmail.com>
2022-05-30 18:48:01 -0400

    feat(auth): update authorizaton method, replace sanctum with password auth

--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,10 @@ Route::prefix('/auth')->name('auth.')->group(function () {
 
 Route::prefix('/stores')->name('stores.')->group(function () {
     Route::get('/', [StoresController::class, 'index'])->name('index');
-    Route::post('/', [StoresController::class, 'store'])->name('store')->middleware('auth:sanctum');
+    Route::post('/', [StoresController::class, 'store'])->name('store')->middleware('auth:api');
     Route::get('/{store}', [StoresController::class, 'show'])->name('show');
-    Route::put('/{store}', [StoresController::class, 'update'])->name('update')->middleware('auth:sanctum');
-    Route::delete('/{store}', [StoresController::class, 'destroy'])->name('destroy')->middleware('auth:sanctum');
+    Route::put('/{store}', [StoresController::class, 'update'])->name('update')->middleware('auth:api');
+    Route::delete('/{store}', [StoresController::class, 'destroy'])->name('destroy')->middleware('auth:api');
 });
 
 Route::prefix('/store-reviews')->name('storeReviews.')->group(function () {
@@ -23,7 +23,7 @@ Route::prefix('/store-reviews')->name('storeReviews.')->group(function () {
     Route::get('/count', [StoreReviewsController::class, 'count']);
 });
 
-Route::prefix('/users')->name('users.')->middleware('auth:sanctum')->group(function () {
+Route::prefix('/users')->name('users.')->middleware('auth:api')->group(function () {
     Route::get('/', [UsersController::class, 'index'])->name('index');
     Route::post('/', [UsersController::class, 'store'])->name('store');
     Route::get('/{user}', [UsersController::class, 'show'])->name('show');
